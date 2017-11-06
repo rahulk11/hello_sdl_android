d48e8238c111b64543a182cdab340b4e01751368
rahul kumar <rahul.k11@hotmail.com>
2017-11-06 16:12:26 +0530

    Initial commit

---
+++ app/build/generated/source/buildConfig/androidTest/usb/debug/com/sdl/hellosdlandroid/test/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sdl.hellosdlandroid.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.sdl.hellosdlandroid.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "usb";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from product flavor: usb
+  public static final String SECURITY = "OFF";
+  public static final String TRANSPORT = "USB";
+}
---
+++ app/build/generated/source/buildConfig/tcp/debug/com/sdl/hellosdlandroid/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sdl.hellosdlandroid;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.sdl.hellosdlandroid";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "tcp";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from product flavor: tcp
+  public static final String SECURITY = "OFF";
+  public static final String TRANSPORT = "TCP";
+}
